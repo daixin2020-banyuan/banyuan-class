3e0e443d19e869a61f58dc82b04c684c7e6cedfc
edz <edz@DaiXindeMacBook-Air.local>
2020-08-17 19:21:22 +0800

    8月15日作业

---
+++ 第三阶段/注册登陆作业/server/controller/student.js
@@ -0,0 +1,43 @@
+const services = require ('../services/student')
+const _ = require ('lodash')
+
+/* 
+ *  student 页面
+*/
+async function student(ctx,next){
+
+  const data = ctx.request.query
+
+  const students = await services.getStudent(data)
+
+  console.log(students)
+
+  ctx.state = {
+    students
+  }
+
+  await ctx.render('students',ctx.state)
+}
+
+/* 
+ * 增加学生
+*/
+async function addStudent(ctx,next){
+
+  const data = ctx.request.body
+
+  //添加学生
+  let result = await services.addStudent(data)
+
+  result.students = await services.getStudent({})
+  
+
+  ctx.response.body = result
+ 
+}
+
+
+module.exports = {
+  student,
+  addStudent
+}
